fc8d0e59947dcda2478f8613285fb9eaf423704c
Paf <pavlinpenevv@abv.bg>
2022-12-09 21:30:49 +0200

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  webApiBaseUrl: 'https://store-ge-api-prod.azure-websites.net',
+  cookieBaseDomain: 'https://store-ge-api-prod.azure-websites.net',
+};
